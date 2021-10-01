7ea0abc4495685b49970e2c627eeb295d0ccb9d7
Ismael Morante <ismael.morante3@gmail.com>
2021-10-01 20:00:21 +0200

    feat: :sparkles: first configuration files

--- app/config.py
+++ app/config.py
@@ -1,20 +1,17 @@
-from decouple import config
-
+import os
 class Config(object):
+    DEBUG = False
     SECRET_KEY = "\xc6\xebR\xf1\xf3\xcf\x08)\x89\xdeX\xcd\xc6\xa1a\xaa\x05\xeb6\xb5\x996\xb6"
     SESSION_COOKIE_SECURE = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 
 class DevelopmentConfig(Config):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/users'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    SESSION_COOKIE_SECURE = False
+
 
 class ProductionConfig(Config):
-    DEBUG = False
-    SQLALCHEMY_DATABASE_URI = config('DATABASE_URL', default='localhost')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-config = {
-    'development': DevelopmentConfig,
-    'production': ProductionConfig
-}
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', None)
--- app/main.py
+++ app/main.py
@@ -1,7 +1,45 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_sslify import SSLify
+import os
+
+db = SQLAlchemy()
 
 
-@app.route('/')
-def index():
-    return "<h1>Welcome to CodingX</h1>"
+def page_not_found(e):
+    print('Error', e)
+    return redirect(url_for('main.index'))
+
+def create_app():
+    app = Flask(__name__, static_url_path='/static')
+    is_prod = os.environ.get('PRODUCTION', None)
+    if is_prod:
+        app.config.from_object("app.config.ProductionConfig")
+    else:
+        app.config.from_object("app.config.DevelopmentConfig")
+
+    db.init_app(app)
+    sslify = SSLify(app, subdomains=True)
+    app.register_error_handler(500, page_not_found)
+    app.register_error_handler(404, page_not_found)
+
+    # blueprint for auth routes in our app
+    # from blueprints.auth import auth as auth_blueprint
+    # app.register_blueprint(auth_blueprint)
+
+    # blueprint for non-auth parts of app
+    from app.blueprints.main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    # login_manager = LoginManager(app)
+    # login_manager.login_view = 'auth.login'
+    # login_manager.init_app(app)
+
+    # from models import User
+    # @login_manager.user_loader
+    # def load_user(user_id):
+    #     return User.query.get(int(user_id))
+
+    return app
+
+app = create_app()
